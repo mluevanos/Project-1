52ad91512e75b2e58b4768960085b8dbdcfc5c6e
Crash Override <emmettmathison@gmail.com>
2019-11-01 12:26:35 -0700

    final?

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Project-1
 Project 1
+
+* Must use at least two server-side APIs
+World Weather Online
+Edamam recipe API
+
+* Must use a CSS framework other than Bootstrap
+Foundation
+
+* Must be interactive (i.e: accept and respond to user input)
+Responds to user sip input
+
+* Must have a polished UI
+Background related to the weather condition
+
+* Must meet good quality coding standards
+Console logged and consistency
+
+* Does not use alerts, confirms or prompts (look into modals)
+* Must be deployed to GitHub Pages
+Will deploy
+
+ Elevator pitch: a one minute description of your application
+skyTunes brings customized recipes that are based on weather conditions in your specific region. 
+Overall, people are cooking more and more according to nutrition journal, especially among men. 
+We avoid choice overload by curating recipe options according to environmental factors, increasing the chance that one of the recipes would be selected
+We are creating a concept brand based on users environmental factors, something that may not always be considered
+
+* Concept: What is your user story? What was your motivation for development?
+As a budget conscious family, I need help figuring out healthy and affordable meal plans for my family that reduces our dependencies on processed foods
+
+
+* Process: What were the technologies used? How were tasks and roles broken down and assigned? What challenges did you encounter? What were your successes?
+We used 2 api’s and input fields to generate results
+We faced knowledge gaps, one person on the team took care of a majority of the back end
+We pivoted on music playlists based on weather conditions due to complex api’s
+Successes were design elements that Emmett implemented 
+We also had a backup api at the ready in case music API could not work
+
+* Demo: Show your stuff!
+Visit the website, enter your zip code, and pull up your weather condition and a curated list of recipes
+
+* Directions for Future Development
+SkyTunes would love to provide a 5 day forecast with curated meal plans based on weather for the week or day
+We would love to add search parameters for recipes based on tastes, food allergies, nutrition needs, etc
+Additionally, SkyTunes is a lifestyle brand based on weather. We would love to add features for holidays, vegetarians/vegans
+We want to add playlists based on weather conditions down the line, with the option to select playlists for cooking events: holidays, tailgating, family bbqs, Sunday night dinners, birthdays, etc
+We plan to bring in revenue based on advertisements
+
+* Links to the deployed application and the GitHub repository
+https://github.com/mluevanos/Project-1.git
+https://mluevanos.github.io/Project-1/
--- script.js
+++ script.js
@@ -1,57 +1,160 @@
-var tellMe = document.querySelector("#tellMe");
-var useME = document.querySelector("#useME");
-
-
-$(tellMe).on("click", function() {
-    $(".container1").remove();
-    $(".container2").remove();
-    $(".container3").show();
+var APIKey = "4aa84673b53e416b8d8183934192910";
+  
+    // Here we are building the URL we need to query the database
+var queryURL = "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=4aa84673b53e416b8d8183934192910&q=Los Angeles&format=json&num_of_days=1&extra=localObs,utcDateTime&date=today&includelocation=yes" + APIKey;
+  
+    // Here we run our AJAX call
+$.ajax({
+    url: queryURL,
+    method: "GET"
 })
+      // We store all of the retrieved data inside of an object called "response"
+.then(function(response) {
+        // Log the queryURL
+        console.log(queryURL);
+        // Log the resulting object
+        console.log(response);
+});
 
-$(useME).on("click", function() {
-    $(".container1").remove();
-    $(".container2").remove();
+$("#confirm").on("click", function() {
     $(".container3").show();
+    var locationInput = document.querySelector(".myLocation").value;
+    console.log(locationInput);
+    var queryTest = "http://api.worldweatheronline.com/premium/v1/weather.ashx?key="+APIKey+"&q="+locationInput+"&format=json&num_of_days=1&includelocation=yes";
+    $.ajax({
+        url: queryTest,
+        method: "GET"
+    })
+          // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+            // Log the queryURL
+            console.log(queryURL);
+            // Log the resulting object
+            console.log(response);
+            var tempNow = response.data.current_condition[0].temp_F;
+            var descNow = response.data.current_condition[0].weatherCode;
+            var conditionStore = response.data.current_condition[0].weatherDesc[0].value;
+            console.log(descNow);
+            console.log(tempNow);
+            var currentDesc = response.data.current_condition[0].weatherDesc[0].value;
+            var tempIcon = response.data.current_condition[0].weatherIconUrl[0].value;
+            console.log(tempIcon);
+            var tempIconImg = $("<img>").attr("src", tempIcon);
+            
+            $(".openWeather").prepend("<br><hr>" + "Current Temperature : " + tempNow + "F" + "<br>" + "Current Condition : " + conditionStore );
+            $(".openWeather").append(tempIconImg);
+            if (descNow === "113"){
+                $("#myVideo6").show();
+                $("#myVideo").hide();
+                var urlHot = "https://api.edamam.com/search?q=salad&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+                $.ajax({
+                url: urlHot,
+                method: "GET"
+                })
+                .then(function(response) {
+                    console.log(response);
+                    var recipeList = response.hits;
+                    console.log(recipeList[0].recipe.label);
+                    console.log(recipeList[0].recipe.shareAs); //OR recipe.shareAs
+                    for (let i=0; i<recipeList.length; i++){
+                        var picDiv = $("<div>");
+                        var recipeName = recipeList[i].recipe.label;
+                        var recipeUrl = recipeList[i].recipe.shareAs;
+                        var recipeImg = recipeList[i].recipe.image;
+                        console.log(recipeImg);
+                        var recipeText = recipeName.link(recipeUrl);
+                        var recipeIcon = $("<img>").attr("src", recipeImg);
+                        picDiv.prepend("<br><hr>" + recipeText);
+                        picDiv.append(recipeIcon)
+                        $(".food").append(picDiv);
+                    }
+                })
+            }
+            else if(descNow ==="323" || descNow ==="326" || descNow ==="329" || descNow ==="332" || descNow ==="335" || descNow ==="338"){
+                var urlSnow = "https://api.edamam.com/search?q=soup&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+                $("#myVideo5").show();
+                $("#myVideo").hide();
+                $.ajax({
+                url: urlSnow,
+                method: "GET"
+                })
+                .then(function(response) {
+                    console.log(response);
+                    var recipeList = response.hits;
+                    console.log(recipeList[0].recipe.label);
+                    console.log(recipeList[0].recipe.shareAs); //OR recipe.shareAs
+                    for (i=0; i<recipeList.length; i++){
+                        var recipeName = recipeList[i].recipe.label;
+                        var recipeUrl = recipeList[i].recipe.shareAs;
+                        var recipeText = recipeName.link(recipeUrl);
+                        $(".food").prepend("<br><hr>" + recipeText);
+                    }
+                })
+            }
+            else if(descNow ==="308" || descNow ==="305" || descNow ==="302" || descNow ==="299" || descNow ==="296" || descNow ==="293"){
+                $("#myVideo2").show();
+                $("#myVideo").hide();
+                var urlRain = "https://api.edamam.com/search?q=crockpot&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+                $.ajax({
+                url: urlRain,
+                method: "GET"
+                
+                })
+                .then(function(response) {
+                    console.log(response);
+                    var recipeList = response.hits;
+                    console.log(recipeList[0].recipe.label);
+                    console.log(recipeList[0].recipe.shareAs); //OR recipe.shareAs
+                    for (i=0; i<recipeList.length; i++){
+                        var recipeName = recipeList[i].recipe.label;
+                        var recipeUrl = recipeList[i].recipe.shareAs;
+                        var recipeText = recipeName.link(recipeUrl);
+                        $(".food").prepend("<br><hr>" + recipeText);
+                    }
+                })
+            }
+            else if(descNow === "122") {
+                var urlOvercast = "https://api.edamam.com/search?q=roast&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+                $("#myVideo4").show();
+                $("#myVideo").hide();
+                $.ajax({
+                url: urlOvercast,
+                method: "GET"
+                })
+                .then(function(response) {
+                    console.log(response);
+                    var recipeList = response.hits;
+                    console.log(recipeList[0].recipe.label);
+                    console.log(recipeList[0].recipe.shareAs); //OR recipe.shareAs
+                    for (i=0; i<recipeList.length; i++){
+                        var recipeName = recipeList[i].recipe.label;
+                        var recipeUrl = recipeList[i].recipe.shareAs;
+                        var recipeText = recipeName.link(recipeUrl);
+                        $(".food").prepend("<br><hr>" + recipeText);
+                    }
+                })
+            }
+
+            else {
+                $("#myVideo4").show();
+                $("#myVideo").hide();
+                var urlDessert = "https://api.edamam.com/search?q=dessert&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+                $.ajax({
+                url: urlDessert,
+                method: "GET"
+                })
+                .then(function(response) {
+                    console.log(response);
+                    var recipeList = response.hits;
+                    console.log(recipeList[0].recipe.label);
+                    console.log(recipeList[0].recipe.shareAs); //OR recipe.shareAs
+                    for (i=0; i<recipeList.length; i++){
+                        var recipeName = recipeList[i].recipe.label;
+                        var recipeUrl = recipeList[i].recipe.shareAs;
+                        var recipeText = recipeName.link(recipeUrl);
+                        $(".food").prepend("<br><hr>" + recipeText);
+                    }
+                })
+            }
     
-}
-) 
-
-
-
-
-
-var temperature = data.main.temp * (9/5) - 459.67;
-
-// // var weathers = { sunny :"sunny",
-// //                  cloudy: "cloudy",
-// //                  mostlyCloudy: "mostly cloudy",
-// //                  windy: "windy",
-// //                  rainy: "rainy",
-// //                  snowy: "snowy",
-// //                  monsoony: "monsoony", 
-// //                  icy: "icy",
-
-
-var data = "whatever the ajax call for for open weather api is"
-
-if (weather[0].description = "clear sky") {
-    brIMG = sunny
-}
-
-if (weather[0].description = "clear sky" && wind.speed>15) {
-    document.body.style.background-image = url('SUNNYWINDY');
-}
-
-
-// // clear sky: https://www.youtube.com/watch?v=VLrk-EV_EGA 
-
-// // descriptions: broken clouds, few clouds, scattered cloud,  overcast clouds,
-// // heavy intensity rain, light rain, moderate rain,
-
-// // broken clouds / few clouds / scattered clouds background video: https://www.youtube.com/watch?v=joQ42CYhtZw
-// //play at 2x speed 
-// // use at different points in video, different cloud densities 
-// // or https://www.youtube.com/watch?v=YgmIibSnZs0
-
-// // overcast clouds: https://www.youtube.com/watch?v=Wimkqo8gDZ0
-// //
+    })})
